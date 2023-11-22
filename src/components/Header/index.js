aabede821ebbf37ef759dfb29f8a16cf766acfa9
nareshkumar <vislavathnareshkumar52@gmail.com>
2023-11-22 06:16:19 +0000

    solution

---
+++ src/components/Header/index.js
@@ -0,0 +1,127 @@
+import Cookies from 'js-cookie'
+import {Link, withRouter} from 'react-router-dom'
+import './index.css'
+
+const Header = props => {
+  const onClickLogout = () => {
+    const {history} = props
+    Cookies.remove('jwt_token')
+    history.replace('/login')
+  }
+  const Logout = () => {
+    const {history} = props
+    Cookies.remove('jwt_token')
+    history.replace('/login')
+  }
+
+  return (
+    <nav className="nav-header">
+      <div className="nav-content">
+        <div className="nav-bar-mobile-logo-container">
+          <img
+            className="website-logo"
+            src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-logo-img.png"
+            alt="website logo"
+          />
+
+          <button type="button" className="nav-mobile-btn">
+            <img
+              onClick={Logout}
+              src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-log-out-img.png"
+              alt="nav logout"
+              className="nav-bar-img"
+            />
+          </button>
+        </div>
+
+        <div className="nav-content nav-bar-large-container">
+          <img
+            className="website-logo"
+            src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-logo-img.png"
+            alt="website logo"
+          />
+          <ul className="nav-menu">
+            <li className="nav-menu-item">
+              <Link className="nav-link" to="/">
+                Home
+              </Link>
+            </li>
+
+            <li className="nav-menu-item">
+              <Link className="nav-link" to="/products">
+                Products{' '}
+              </Link>
+            </li>
+
+            <li className="nav-menu-item">
+              <Link className="nav-link" to="/cart">
+                Cart{' '}
+              </Link>
+            </li>
+          </ul>
+          <button
+            type="button"
+            className="logout-desktop-btn"
+            onClick={onClickLogout}
+          >
+            Logout
+          </button>
+        </div>
+      </div>
+      <div className="nav-menu-mobile">
+        <ul className="nav-menu-list-mobile">
+          <li className="nav-menu-item-mobile">
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-home-icon.png"
+              alt="nav home"
+              className="nav-bar-img"
+            />
+          </li>
+
+          <li className="nav-menu-item-mobile">
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-icon.png"
+              alt="nav products"
+              className="nav-bar-img"
+            />
+          </li>
+
+          <li className="nav-menu-item-mobile">
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-cart-icon.png"
+              alt="nav cart"
+              className="nav-bar-img"
+            />
+          </li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
+export default withRouter(Header)
+
+/*
+5 test cases are failed that are related to the Logout button.
+Mistake:
+
+
+
+When clicking the Logout button, the page redirected to the /login route using the history.replace() method.
+
+
+
+Even though 5 test cases are failed that are related to the Logout button.
+
+
+
+Solution:
+
+
+
+We have to apply the onClick attribute for the Logout button in the small devices.
+
+
+
+So, when we click on the Logout button on the small devices, the page should be redirected to the /login page.
+
+*/
